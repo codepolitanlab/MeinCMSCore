468b47eb5b322a7599189a72b72ba06f518d0bc0
Toni Haryanto <toha.samba@gmail.com>
2020-04-18 19:04:59 +0700

    add functions for enabling psr4

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace Meincms;
+
+if (! function_exists('Meincms\setupPsr4')) {
+    /**
+     * Helper for defining psr4
+     *
+     */
+    function setupPsr4()
+    {
+        // Only Autoload PHP Files
+        spl_autoload_extensions('.php'); 
+
+        // available namespace under \app and \meincms
+        spl_autoload_register(function($classname)
+        {
+            if( strpos($classname,'\\') !== false )
+            {
+                // Namespaced Classes
+                $classfile = str_replace('\\','/',$classname).'.php';
+                $class_segment = explode('/', $classfile);
+
+                $scope = array_shift($class_segment);
+
+                if($classname[0] !== '/') 
+                {
+
+                    if($scope == 'app')
+                    {
+                        $classfile = SITEPATH.implode('/', $class_segment);
+                        require_once($classfile);
+                    }
+
+                    elseif($scope == 'shared')
+                    {
+                        $classfile = 'shared/'.implode('/', $class_segment);
+                        require_once($classfile);
+                    }
+
+                    else if($scope == 'Core')
+                    {
+                        $classfile = COREPATH.implode('/', $class_segment);
+                        require_once $classfile;
+                    }
+                }
+            }
+        });
+    };
+}
