3420ec19c789d70b2e524042054db0fd98261059
Toni Haryanto <toha.samba@gmail.com>
2020-04-27 15:55:08 +0700

    Call default _display() method

--- src/Hooks/DisplayOverrideHook.php
+++ src/Hooks/DisplayOverrideHook.php
@@ -2,4 +2,14 @@
 
 namespace Meincms\Hooks;
 
-class DisplayOverrideHook extends BaseHook {}
+class DisplayOverrideHook extends BaseHook {
+
+	protected $methods = [
+		'display'
+	];
+
+	public function display()
+	{
+		ci()->output->_display();
+	} 
+}
